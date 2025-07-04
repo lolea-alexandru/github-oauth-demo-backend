47ee8b335bbd70f8228973f6596bba1fa92a8866
lolea-alexandru <alexandru.n.lolea@gmail.com>
2025-07-04 13:55:45 +0200

    ✨Added endpoint for retrieving the access token. The endpoint is thus named "/getAccessToken"

--- index.js
+++ index.js
@@ -10,9 +10,39 @@ const CLIENT_ID = "Ov23li2DUsGWngQFU7q5";
 const app = express();
 
 // Register middlewares
-app.use(cors);
+app.use(cors());
 app.use(bodyParser.json());
 require("dotenv").config();
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.get("/getAccessToken", async (req, res) => {
+  // Request configuration
+  const accessTokenURL = "https://github.com/login/oauth/access_token";
+  const params = `?client_id=${CLIENT_ID}&client_secret=${process.env.GITHUB_CLIEN_SECRET}&code=${req.query.code}`;
+  const headers = {
+    Accept: "application/json",
+  };
+
+  try {
+    // Retrieve the access token from the GitHub server
+    const response = await axios.post(
+      accessTokenURL + params,
+      {},
+      {
+        headers: headers,
+      }
+    );
+
+    // Send the access token back to the frontend
+    res.send(response.data.access_token).status(200);
+  } catch (error) {
+    // TODO: Add better error handling
+    res.send(error).status(400);
+  }
+});
 
 // Start the server
 app.listen(process.env.PORT || 4000, () => {
