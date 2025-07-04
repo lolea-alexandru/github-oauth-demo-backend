8396f725d1133bfd10511b20c3f4775b9e851e57
lolea-alexandru <alexandru.n.lolea@gmail.com>
2025-07-04 16:01:06 +0200

    🎨 split github asnd gitlab functionality into two different routers

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-const CLIENT_ID = "Ov23li2DUsGWngQFU7q5";
+/* ============================== IMPORTS ============================== */
+// core
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+const bodyParser = require("body-parser");
+
+// routers
+const githubRouter = require("./routers/github");
+
+/* ============================== CONFIG ============================== */
+
+const app = express();
+
+// Register middlewares
+app.use(cors());
+app.use(bodyParser.json());
+require("dotenv").config();
+
+// Register routers
+app.use("/github", githubRouter);
+
+// TODO: use Swagger to document the endpoint
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+// Start the server
+app.listen(process.env.PORT || 4000, () => {
+  console.log(`Server is running on port ${process.env.PORT || 4000}`);
+});
