2b7ea28eb9da13cad274bc59b8a1a5812f09cd4f
lolea-alexandru <alexandru.n.lolea@gmail.com>
2025-07-04 13:03:59 +0200

    ✨📝 Started the server and documented the .env varaibles necessary

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+/* ============================== IMPORTS ============================== */
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+const bodyParser = require("body-parser");
+
+/* ============================== CONFIG ============================== */
 const CLIENT_ID = "Ov23li2DUsGWngQFU7q5";
+
+const app = express();
+
+// Register middlewares
+app.use(cors);
+app.use(bodyParser.json());
+require("dotenv").config();
+
+// Start the server
+app.listen(process.env.PORT || 4000, () => {
+  console.log(`Server is running on port ${process.env.PORT || 4000}`);
+});
